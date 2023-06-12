524deb9c868bbd3c60295f34ea24f789b672d59d
matt <120654723+MuhdMokhtar@users.noreply.github.com>
2023-06-12 21:32:21 +0800

    update

--- app/Controller & Model/Controller/Incentive.php
+++ app/Controller & Model/Controller/Incentive.php
@@ -19,7 +19,7 @@ class Incentive extends Controller{
         if(isset($_SESSION['user_id'])){
             $data['title'] = 'Permohonan Insentif Khas Pasangan Pengantin';
             $this->view('/Template/headerModule5', $data);
-            $this->view('UserView/UserApplyIncentive', $data);
+            $this->view('Manage Incentive/UserApplyIncentive', $data);
             $this->view('/Template/footer');
         } else {
             header('Location: ' . BASEURL . '/LogIn');
@@ -32,7 +32,7 @@ class Incentive extends Controller{
         if(isset($_SESSION['user_id'])){
             $data['title'] = 'Sejarah Permohonan Insentif Khas Pasangan Pengantin';
             $this->view('/Template/headerModule5', $data);
-            $this->view('UserView/UserIncentiveAppHistory', $data);
+            $this->view('Manage Incentive/UserIncentiveAppHistory', $data);
             $this->view('/Template/footer');
         } else {
             header('Location: ' . BASEURL . '/LogIn');
--- app/View/Manage Incentive/UserApplyIncentive.php
+++ app/View/Manage Incentive/UserApplyIncentive.php
@@ -246,7 +246,7 @@
                                 <div class="button-container">
                                     <input type="button" class="btn btn-danger" value="HANTAR" onclick="showSuccessModal()">
                                     <input type="button" class="btn btn-primary" onclick="GeneratePdf();" value="CETAK">
-                                    <a class="btn btn-primary"  style="float: right;" href="<?= BASEURL ?>/Incentive" role="button">Kembali</a>
+
                                 </div>
                             </form>
 
