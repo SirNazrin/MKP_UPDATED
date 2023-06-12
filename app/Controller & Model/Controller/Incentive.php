9f38e46d3fa81afc6b5a64808acdab56e7a5edb8
matt <120654723+MuhdMokhtar@users.noreply.github.com>
2023-06-12 19:54:49 +0800

    update

--- app/Controller & Model/Controller/Incentive.php
+++ app/Controller & Model/Controller/Incentive.php
@@ -19,11 +19,7 @@ class Incentive extends Controller{
         if(isset($_SESSION['user_id'])){
             $data['title'] = 'Permohonan Insentif Khas Pasangan Pengantin';
             $this->view('/Template/headerModule5', $data);
-<<<<<<< Updated upstream
             $this->view('UserView/UserApplyIncentive', $data);
-=======
-            $this->view('Manage Incentive/UserApplyIncentiveInterface', $data);
->>>>>>> Stashed changes
             $this->view('/Template/footer');
         } else {
             header('Location: ' . BASEURL . '/LogIn');
@@ -36,11 +32,7 @@ class Incentive extends Controller{
         if(isset($_SESSION['user_id'])){
             $data['title'] = 'Sejarah Permohonan Insentif Khas Pasangan Pengantin';
             $this->view('/Template/headerModule5', $data);
-<<<<<<< Updated upstream
             $this->view('UserView/UserIncentiveAppHistory', $data);
-=======
-            $this->view('Manage Incentive/UserViewIncentiveInterface', $data);
->>>>>>> Stashed changes
             $this->view('/Template/footer');
         } else {
             header('Location: ' . BASEURL . '/LogIn');
